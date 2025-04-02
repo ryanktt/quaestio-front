727aad9449f9dd57d09e49f01a52beda002d7abc
Ryan <ryankayrorv@outlook.com>
2025-04-01 15:38:31 -0900

    fix cookie settings

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -54,10 +54,10 @@ export default function Auth() {
 
 		setCookie('authData', authResponse, {
 			path: '/',
-			httpOnly: import.meta.env.VITE_MODE === 'production',
+			httpOnly: false,
 			secure: import.meta.env.VITE_MODE === 'production',
 			expires: new Date(session.expiresAt),
-			sameSite: 'strict',
+			sameSite: 'none',
 		});
 		globalContext.state.login(user, session, authToken);
 		navigate('/board/questionnaires');
@@ -78,7 +78,13 @@ export default function Auth() {
 	return (
 		<form onSubmit={form.onSubmit(handleFormSubmit)}>
 			{type === 'SIGNUP' ? (
-				<TextInput {...form.getInputProps('name')} required label="Name" placeholder="your name" mt="sm" />
+				<TextInput
+					{...form.getInputProps('name')}
+					required
+					label="Name"
+					placeholder="your name"
+					mt="sm"
+				/>
 			) : null}
 			<TextInput
 				{...form.getInputProps('email')}
@@ -90,7 +96,14 @@ export default function Auth() {
 			/>
 			<Password onPasswordChange={handlePasswordChange} comfirmPassword={type === 'SIGNUP'} />
 			<Center>
-				<Button style={{ width: '100%' }} justify="center" size="sm" mt="xl" type="submit" variant="gradient">
+				<Button
+					style={{ width: '100%' }}
+					justify="center"
+					size="sm"
+					mt="xl"
+					type="submit"
+					variant="gradient"
+				>
 					{type === 'SIGNUP' ? 'Sign Up' : 'Log In'}
 				</Button>
 			</Center>
